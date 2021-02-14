6cf12726a05443125817d9259219a9efe0601bb5
My Siira <my_siira@hotmail.com>
2021-02-14 22:36:52 +0100

    Added some minor styling to create recipe with url

--- src/components/CreateRecipeWithUrl.js
+++ src/components/CreateRecipeWithUrl.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import useCreateUrlRecipe from '../hooks/useCreateUrlRecipe';
+import placeholder from '../images/placeholder.png';
 
 const CreateRecipeWithUrl = () => {
 
@@ -50,52 +51,58 @@ const CreateRecipeWithUrl = () => {
         <>
             <h1 className="page__title">Skapa recept</h1>
             <p className="page__text">Steg 2 av 2</p>
-            <form onSubmit={handleSubmit}>
+            <form className="recipe-form" onSubmit={handleSubmit}>
 
-                <div className="">
-                    <label htmlFor="name" className="form-label">Receptnamn *</label>
-                    <input 
-                        type="text"     
-                        className="" 
-                        id="name" 
-                        required
-                        onChange={handleInput}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="url" className="">Länk *</label>
-                    <input 
-                        type="url" 
-                        className="" 
-                        id="url" 
-                        required
-                        onChange={handleInput}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="comment" className="">Kommentar</label>
-                    <textarea 
-                        name="comment" 
-                        className="" 
-                        id="comment" 
-                        onChange={handleInput}
-                    >
-                    </textarea>
-                </div>
+                <div className="recipe-form__content">
+                    <img className="recipe-form__image" src={placeholder} alt="placeholder-image"/>
 
-                <div className="mb-3">
-                    <label htmlFor="photo" className="">Bild</label>
-                    <input 
-                        type="file" 
-                        className="form-control" 
-                        id="photo" 
-                        onChange={handleFileChange}
-                    />
-                </div>
+                    <div className="recipe-form__inputs">
+                        <div className="mb-3">
+                            <label htmlFor="url" className="">Länk *</label>
+                            <input 
+                                type="url" 
+                                className="" 
+                                id="url" 
+                                required
+                                onChange={handleInput}
+                            />
+                        </div>
+                        <div className="">
+                            <label htmlFor="name" className="form-label">Receptnamn *</label>
+                            <input 
+                                type="text"     
+                                className="" 
+                                id="name" 
+                                required
+                                onChange={handleInput}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="comment" className="">Kommentar</label>
+                            <textarea 
+                                name="comment" 
+                                className="" 
+                                id="comment" 
+                                onChange={handleInput}
+                            >
+                            </textarea>
+                        </div>
 
-                <div>
-                    <input type="checkbox" name="Veganskt" onChange={handleCheckbox}/>
-                    <label>Veganskt</label>
+                        <div className="mb-3">
+                            <label htmlFor="photo" className="">Bild</label>
+                            <input 
+                                type="file" 
+                                className="form-control" 
+                                id="photo" 
+                                onChange={handleFileChange}
+                            />
+                        </div>
+
+                        <div>
+                            <input type="checkbox" name="Veganskt" onChange={handleCheckbox}/>
+                            <label>Veganskt</label>
+                        </div>
+                    </div>
                 </div>
 
                 <button type="submit" className="">Skapa recept</button>
