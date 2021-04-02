58e97542a22a052236042b31f92f05b411fffe6b
My Siira <my@peter-simonsson.local>
2021-04-02 18:53:02 +0200

    Change profile icon and make some changes to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,26 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { faSortDown } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as User } from "../assets/user.svg";
 
 const Navbar = () => {
     const { currentUser } = useAuth();
+    const [showDropdown, setShowDropdown] = useState(false);
 
     return (
         <nav className="navbar">
-            <div>
                 <Link to={`/`} className="navbar__logo">
-                    <Logo/>
+                    <Logo />
                     <p className="navbar__logotext">my veggie recipes</p>
                 </Link>
-            </div>
+
 
             <ul className={`navbar__nav-items`}>
-
                 {currentUser ? (
                     <>
+                    <div className="navbar__nav">
                         <li className="navbar__nav-item">
                             <NavLink
                                 to={`/my-recipes`}
@@ -39,23 +38,47 @@ const Navbar = () => {
                                 Skapa recept
                             </NavLink>
                         </li>
-                        <button
-                            className="navbar__profile-button"
-                            aria-expanded="false"
-                        >
-                            <FontAwesomeIcon icon={faUserCircle} className="navbar__profile-icon"/>
-                            <FontAwesomeIcon icon={faSortDown} />
-                        </button>
-                        <ul className="navbar__dropdown">
-                            <li className="navbar__nav-item">
-                                <NavLink
-                                    to={`/logout`}
-                                    className="navbar__nav-link"
+                        </div>
+                        <li className="navbar__nav-item navbar__nav-item-dropdown">
+                            <button
+                                className="navbar__profile-button"
+                                aria-expanded="false"
+                                onClick={() => setShowDropdown(prevState => !prevState)}
                                 >
-                                    Logga ut
-                                </NavLink>
-                            </li>
-                        </ul>
+                                <User className="navbar__profile-icon"/>
+                            </button>
+                            {
+                                showDropdown &&
+
+                                <ul className="navbar__dropdown">
+                                <li className="navbar__nav-item navbar__nav-item--mobile">
+                                    <NavLink
+                                        to={`/my-recipes`}
+                                        className="navbar__nav-link"
+                                    >
+                                        Mina recept
+                                    </NavLink>
+                                </li>
+                                <li className="navbar__nav-item navbar__nav-item--mobile">
+                                    <NavLink
+                                        to={`/my-recipes/create-recipe`}
+                                        className="navbar__nav-link"
+                                    >
+                                        Skapa recept
+                                    </NavLink>
+                                </li>
+                                <li className="navbar__nav-item">
+                                    <NavLink
+                                        to={`/logout`}
+                                        className="navbar__nav-link"
+                                    >
+                                        Logga ut
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            }
+                            
+                        </li>
                     </>
                 ) : (
                     <li className="navbar__nav-item">
