a85c644a730bda7ff85f5aa62e869298aae11d4c
My Siira <my@peter-simonsson.local>
2021-04-03 13:48:11 +0200

    Filter functionality done

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -10,10 +10,7 @@ const AllRecipes = () => {
 
     const getRecipes = async () => {
         const myRecipes = [];
-
-        let snapshot;
-        console.log('vegan', vegan);
-        
+        let snapshot;    
 
         if(vegan) {
             snapshot = await db
@@ -52,12 +49,11 @@ const AllRecipes = () => {
                 Recept <Broccoli className="icon" />
             </h1>
             <ul className="filter">
-
-                <li className="filter__item" onClick={handleFilterSearch}>
+                <li className={`filter__item ${!vegan ?  'filter__item--active' : ''}`} onClick={handleFilterSearch}>
                     Alla
                 </li>
                 <li
-                    className="filter__item"
+                    className={`filter__item ${vegan ?  'filter__item--active' : ''}`}
                     id="vegan"
                     onClick={handleFilterSearch}
                 >
@@ -76,88 +72,3 @@ const AllRecipes = () => {
 };
 
 export default AllRecipes;
-
-// import React, { useEffect, useState } from "react";
-// import RecipeCard from "./RecipeCard";
-// import { db } from "../firebase";
-// import AddRecipeButton from "./AddRecipeButton";
-// import { ReactComponent as Broccoli } from "../assets/broccoli.svg";
-
-// const AllRecipes = () => {
-//     const [recipes, setRecipes] = useState([]);
-//     const [active, setActive] = useState('all');
-
-//     const getRecipes = async () => {
-//         const myRecipes = [];
-
-//         const snapshot = await db.collection("recipes").get();
-
-//         snapshot.forEach((doc) => {
-//             myRecipes.push({
-//                 id: doc.id,
-//                 ...doc.data(),
-//             });
-//         });
-
-//         setRecipes(myRecipes);
-//     };
-
-//     useEffect(() => {
-//         getRecipes();
-//     }, []);
-
-//     const getVeganRecipes = async () => {
-//         const myRecipes = [];
-
-//         const snapshot = await db
-//             .collection("recipes")
-//             .where("vegan", "==", true)
-//             .get();
-
-//         snapshot.forEach((doc) => {
-//             myRecipes.push({
-//                 id: doc.id,
-//                 ...doc.data(),
-//             });
-//         });
-
-//         setRecipes(myRecipes);
-//     };
-
-//     const handleFilterSearch = (e) => {
-//         if (e.target.id === "vegan") {
-//             getVeganRecipes();
-//         } else {
-//             getRecipes();
-//         }
-//     };
-
-//     return (
-//         <>
-//             <h1 className="page__title">
-//                 Recept <Broccoli className="icon" />
-//             </h1>
-//             <ul className="filter">
-//                 <li className="filter__item" onClick={handleFilterSearch}>
-//                     Alla
-//                 </li>
-//                 <li
-//                     className="filter__item"
-//                     id="vegan"
-//                     onClick={handleFilterSearch}
-//                 >
-//                     Veganska
-//                 </li>
-//             </ul>
-//             <div className="cards">
-//                 {recipes.length !== 0 &&
-//                     recipes.map((recipe) => (
-//                         <RecipeCard recipe={recipe} key={recipe.id} />
-//                     ))}
-//             </div>
-//             <AddRecipeButton />
-//         </>
-//     );
-// };
-
-// export default AllRecipes;
