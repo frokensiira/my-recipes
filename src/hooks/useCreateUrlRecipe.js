f6b18297a0ef4bd5a65cb7d41229e7a268c808dc
My Siira <my@peter-simonsson.local>
2021-04-04 19:15:48 +0200

    Optimized create recipe code

--- src/hooks/useCreateUrlRecipe.js
+++ src/hooks/useCreateUrlRecipe.js
@@ -14,44 +14,23 @@ const useCreateUrlRecipe = (recipe, submit) => {
             return;
         }
 
-        if (recipe.photoUrl && recipe.fullPath) {
-            //if user uploaded own image, add uploaded photo to database
-            db.collection("recipes")
-                .add({
-                    owner: currentUser.uid,
-                    ownerUsername: currentUser.displayName,
-                    name: recipe.name,
-                    url: recipe.url,
-                    comment: recipe.comment,
-                    path: recipe.fullPath,
-                    photoUrl: recipe.photoUrl,
-                    vegan: recipe.vegan,
-                })
-                .then(() => {
-                    navigate("/my-recipes/");
-                })
-                .catch((err) => {
-                    console.log("something went wrong", err);
-                });
-        } else {
-            //if user chose the image from the url, add uploaded recipe to database
-            db.collection("recipes")
-                .add({
-                    owner: currentUser.uid,
-                    ownerUsername: currentUser.displayName,
-                    name: recipe.name,
-                    url: recipe.url,
-                    comment: recipe.comment,
-                    photoUrl: recipe.photoUrl,
-                    vegan: recipe.vegan,
-                })
-                .then(() => {
-                    navigate("/my-recipes/");
-                })
-                .catch((err) => {
-                    console.log("something went wrong", err);
-                });
-        }
+        db.collection("recipes")
+            .add({
+                owner: currentUser.uid,
+                ownerUsername: currentUser.displayName,
+                name: recipe.name,
+                url: recipe.url,
+                comment: recipe.comment,
+                ...(recipe.fullPath && { path: recipe.fullPath }),
+                photoUrl: recipe.photoUrl,
+                vegan: recipe.vegan,
+            })
+            .then(() => {
+                navigate("/my-recipes/");
+            })
+            .catch((err) => {
+                console.log("something went wrong", err);
+            });
     }, [submit]);
 
     return { error, loading };
