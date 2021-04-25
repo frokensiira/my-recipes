4e76e9ebe1b22022e18c9c5d8748dfd6ee66de54
My Siira <my@peter-simonsson.local>
2021-04-25 17:53:20 +0200

    Add loading to submit button

--- src/components/CreateRecipeWithUrl.js
+++ src/components/CreateRecipeWithUrl.js
@@ -214,7 +214,7 @@ const CreateRecipeWithUrl = () => {
 
                     <VeganCheckbox handleCheckbox={handleCheckbox} recipe={recipe} />
                 
-                    <RecipeSubmitButton>Skapa recept</RecipeSubmitButton>
+                    <RecipeSubmitButton loading={loading}>Skapa recept</RecipeSubmitButton>
                 </div>
             </form>
         </>
