2b29207bd3c47834b3a2b090ca5965428b4e69d5
My Siira <my_siira@hotmail.com>
2021-05-04 23:47:23 +0200

    Fix navbar dropdown click outside problem

---
+++ src/components/Dropdown.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import onClickOutside from "react-onclickoutside";
+import { useAuth } from "../contexts/AuthContext";
+import { ReactComponent as User } from "../assets/user.svg";
+
+function Dropdown () {
+    const { currentUser } = useAuth();
+    const [showDropdown, setShowDropdown] = useState(false);
+    Dropdown.handleClickOutside = () => setShowDropdown(false);
+    return (
+        <li className="navbar__nav-item navbar__nav-item-dropdown">
+            <button
+                className="navbar__profile-button"
+                aria-expanded="false"
+                onClick={() => setShowDropdown((prevState) => !prevState)}
+            >
+                <User className="navbar__profile-icon" />
+                {currentUser && (
+                    <p className="navbar__displayname">
+                        {currentUser.displayName}
+                    </p>
+                )}
+            </button>
+
+            {showDropdown && (
+                <ul className="navbar__dropdown">
+                    <li className="navbar__nav-item navbar__nav-item--mobile">
+                        <NavLink
+                            to={`/my-recipes`}
+                            className="navbar__nav-link"
+                        >
+                            Mina recept
+                        </NavLink>
+                    </li>
+                    <li className="navbar__nav-item navbar__nav-item--mobile">
+                        <NavLink
+                            to={`/my-recipes/create-recipe`}
+                            className="navbar__nav-link"
+                        >
+                            Skapa recept
+                        </NavLink>
+                    </li>
+                    <li className="navbar__nav-item">
+                        <NavLink to={`/logout`} className="navbar__nav-link">
+                            Logga ut
+                        </NavLink>
+                    </li>
+                </ul>
+            )}
+        </li>
+    );
+};
+
+const clickOutsideConfig = {
+    handleClickOutside: () => Dropdown.handleClickOutside,
+};
+
+export default onClickOutside(Dropdown, clickOutsideConfig);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { ReactComponent as Logo } from "../assets/logo.svg";
-import { ReactComponent as User } from "../assets/user.svg";
+import Dropdown from "./Dropdown";
 
 const Navbar = () => {
     const { currentUser } = useAuth();
-    const [showDropdown, setShowDropdown] = useState(false);
 
     return (
         <nav className="navbar">
@@ -46,51 +45,7 @@ const Navbar = () => {
                                 </NavLink>
                             </li>
                         </div>
-                        <li className="navbar__nav-item navbar__nav-item-dropdown">
-                            <button
-                                className="navbar__profile-button"
-                                aria-expanded="false"
-                                onClick={() =>
-                                    setShowDropdown((prevState) => !prevState)
-                                }
-                            >
-                                <User className="navbar__profile-icon" />
-                                {currentUser && (
-                                    <p className="navbar__displayname">
-                                        {currentUser.displayName}
-                                    </p>
-                                )}
-                            </button>
-
-                            {showDropdown && (
-                                <ul className="navbar__dropdown">
-                                    <li className="navbar__nav-item navbar__nav-item--mobile">
-                                        <NavLink
-                                            to={`/my-recipes`}
-                                            className="navbar__nav-link"
-                                        >
-                                            Mina recept
-                                        </NavLink>
-                                    </li>
-                                    <li className="navbar__nav-item navbar__nav-item--mobile">
-                                        <NavLink
-                                            to={`/my-recipes/create-recipe`}
-                                            className="navbar__nav-link"
-                                        >
-                                            Skapa recept
-                                        </NavLink>
-                                    </li>
-                                    <li className="navbar__nav-item">
-                                        <NavLink
-                                            to={`/logout`}
-                                            className="navbar__nav-link"
-                                        >
-                                            Logga ut
-                                        </NavLink>
-                                    </li>
-                                </ul>
-                            )}
-                        </li>
+                        <Dropdown/>
                     </>
                 ) : (
                     <li className="navbar__nav-item">
